09ab3f794d85a76d11d77e8bb1486ab550056f6a
Sebastian Basconcelo <el_basco@hotmail.com>
2023-09-09 01:16:43 -0300

    deploy

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,8 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
 
   base: '/aluraflix/',
-  plugins: [react()]
+  plugins: [react()],
+   build: {
+    outDir: './build'
+  } 
 })
